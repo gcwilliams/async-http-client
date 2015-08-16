8c0c1fb6386db47643fe44fedee6d731d325cbdd
Gareth Williams <gaz_williams@hotmail.com>
2015-08-16 12:44:36 +0100

    removed FunctionalInterface annotation from client, not needed

--- src/main/java/uk/co/gcwilliams/http/AsyncHttpClient.java
+++ src/main/java/uk/co/gcwilliams/http/AsyncHttpClient.java
@@ -2,14 +2,11 @@ package uk.co.gcwilliams.http;
 
 import uk.co.gcwilliams.http.tasks.Task;
 
-import java.util.Map;
-
 /**
  * The async http client interface
  *
  * Created by GWilliams on 06/08/2015.
  */
-@FunctionalInterface
 public interface AsyncHttpClient {
 
     /**
